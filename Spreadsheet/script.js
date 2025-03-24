156bdaf9f75d336da2cbcb528a9ed77d83fd88d4
Andre Pires <erdnapfaserip@gmail.com>
2025-03-24 11:27:53 -0300

    Finish Spreadsheet

--- Spreadsheet/script.js
+++ Spreadsheet/script.js
@@ -28,7 +28,18 @@ const median = (nums) => {
 const spreadsheetFunctions = {
     sum,
     average,
-    median
+    median,
+    even: nums => nums.filter(isEven),
+    firsttwo: nums => nums.slice(0, 2),
+    lasttwo: nums => nums.slice(-2),
+    has2: nums => nums.includes(2),
+    increment: nums => nums.map(num => num + 1),
+    someeven: nums => nums.some(isEven),
+    everyeven: nums => nums.every(isEven),
+    random: ([x, y]) => Math.floor(Math.random() * y + x),
+    range: nums => range(nums[0], nums[1]),
+    nodupes: nums => [...new Set(nums).values()],
+    "": (arg) => arg
 }
 
 const applyFunction = (str) => {
